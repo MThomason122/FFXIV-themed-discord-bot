c813591163a20efd8e0ccd046f8820a43b154a3d
Matthew Thomason <epiccom70@gmail.com>
2020-07-18 16:16:00 -0400

    discord bot WIP

---
+++ FFXIV-themed-discord-bot.py
@@ -0,0 +1,16 @@
+# FFXIV themed discord bot
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('SECRET-TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected.')
+
+client.run(TOKEN)
